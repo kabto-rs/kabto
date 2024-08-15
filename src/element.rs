29a08f1ec47af7590fe4ede99e0142a4de805ffb
kana-rus <kanarus786@gmail.com>
2024-08-16 03:42:18 +0900

    @2024-08-16 03:16+9:00

--- src/element.rs
+++ src/element.rs
@@ -1,6 +1,7 @@
 pub struct Element {
-    tag:   Tag,
-    props: Props,
+    tag:      Tag,
+    props:    Props,
+    children: Vec<Element>,
 }
 
 #[litenum::to]
@@ -10,6 +11,31 @@ enum Tag {
 }
 
 struct Props {
-    value:    Option<String>,
-    children: Vec<Element>,
+    nodeValue: Option<String>,
+}
+
+impl Element {
+    fn JS_render_to(self, container: &web_sys::Node) -> Result<(), web_sys::wasm_bindgen::JsValue> {
+        let document = web_sys::Document::new()?;
+
+        match self.tag {
+            Tag::TEXT_ELEMENT => {
+                let text_node = document.create_text_node("");
+                container.append_child(&text_node)?;
+
+                Ok(())
+            }
+            
+            tag => {
+                let dom = document.create_element(tag.lit())?;
+
+                for child in self.children {
+                    child.JS_render_to(&dom)?;
+                }
+                container.append_child(&dom)?;
+
+                Ok(())
+            }
+        }
+    }
 }
