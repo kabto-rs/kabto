9dadf65b918c60067ab0e65891c002dad0f6a3bd
kana-rus <kanarus786@gmail.com>
2024-08-20 03:44:24 +0900

    next: attributes

---
+++ src/dsl/attributes.rs
@@ -0,0 +1,20 @@
+use crate::vdom::Node;
+
+
+macro_rules! boolean {
+    () => {
+        
+    };
+}
+
+macro_rules! enumerated {
+    () => {
+        
+    };
+}
+
+macro_rules! other {
+    () => {
+        
+    };
+}
