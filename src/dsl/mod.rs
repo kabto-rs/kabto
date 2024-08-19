1f17fa1e46ed798b81a6ca75360a795c29e11452
kana-rus <kanarus786@gmail.com>
2024-08-20 02:54:24 +0900

    @2024-08-20 02:54+9:00

--- src/dsl/mod.rs
+++ src/dsl/mod.rs
@@ -1,93 +1,82 @@
-// use crate::element;
-// use internal::Tag;
-// 
-// 
-// pub type div = Tag<{element::Tag::div}>;
-// pub type h1  = Tag<{element::Tag::h1}>;
-// pub type h2  = Tag<{element::Tag::h2}>;
-// pub type p   = Tag<{element::Tag::p}>;
-// pub type a   = Tag<{element::Tag::a}>;
-// 
-// mod internal {
-//     pub struct Tag<const T: crate::element::Tag>;
-// 
-//     pub struct Element(crate::element::Element);
-// 
-//     macro_rules! attributes {
-//         ($($name:ident)*) => {
-//             impl<const T: crate::element::Tag> Tag<T> {$(
-//                 pub fn $name(mut self, value: ) -> Element {
-// 
-//                 }
-//             )*}
-//             impl Element {$(
-//                 pub fn $name(mut self, value: ) -> Element {
-// 
-//                 }
-//             )*}
-//         };
-//     } attributes! {
-// 
-//     }
-// }
-// 
-
-
-use crate::vdom::{Node, Element};
+use crate::vdom::Node;
 use crate::util::Text;
 
 
-pub trait IntoNode {
-    fn into_node(self) -> Option<Node>;
+pub trait IntoNodes {
+    fn into_nodes(self) -> Nodes;
 }
 
 pub trait NodeCollection: std::marker::Tuple {
-    const N: usize;
-    fn collect(self) -> [Option<Node>; Self::N];
+    fn collect(self) -> impl Iterator<Item = Nodes>;
+}
+
+pub enum Nodes {
+    None,
+    Some(Node),
+    Many(Vec<Node>)
+}
+impl Nodes {
+    fn join_into(self, collection: &mut Vec<Node>) {
+        match self {
+            Self::None    => (),
+            Self::Some(n) => collection.push(n),
+            Self::Many(m) => collection.extend(m)
+        }
+    }
 }
 
 
 //////////////////////////////////////////////
 
 
-impl IntoNode for Node {
-    fn into_node(self) -> Option<Node> {
-        Some(self)
+impl IntoNodes for Node {
+    fn into_nodes(self) -> Nodes {
+        Nodes::Some(self)
     }
 }
-impl<IN: IntoNode> IntoNode for Option<IN> {
-    fn into_node(self) -> Option<Node> {
-        self?.into_node()
+impl<IN: IntoNodes> IntoNodes for Option<IN> {
+    fn into_nodes(self) -> Nodes {
+        match self {
+            None    => Nodes::None,
+            Some(n) => n.into_nodes()
+        }
+    }
+}
+impl<IN: IntoNodes> IntoNodes for Vec<IN> {
+    fn into_nodes(self) -> Nodes {
+        Nodes::Many(self.into_iter()
+            .map(IntoNodes::into_nodes)
+            .fold(Vec::new(), |mut vec, nodes| {nodes.join_into(&mut vec); vec})
+        )
     }
 }
 macro_rules! TextNode {
     ($($text:ty),*) => {$(
-        impl IntoNode for $text {
-            fn into_node(self) -> Option<Node> {
-                Some(Node::Text(Text::from(self).into()))
+        impl IntoNodes for $text {
+            fn into_nodes(self) -> Nodes {
+                Nodes::Some(Node::Text(Text::from(self).into()))
             }
         }
     )*};
 } TextNode! { &'static str, String, u8, usize, i32 }
 
-impl<Children: NodeCollection> FnOnce<Children> for Node
-where [(); Children::N]:
-{
+impl<Children: NodeCollection> FnOnce<Children> for Node {
     type Output = Node;
     extern "rust-call" fn call_once(self, children: Children) -> Self::Output {
         let Node::Element(mut element) = self else {unreachable!()};
-        element.children.extend(children.collect().into_iter().filter_map(|it|it));
+        for nodes in children.collect() {
+            nodes.join_into(&mut element.children);
+        }
         Node::Element(element)
     }
 }
 
 macro_rules! NodeCollection {
     ($($node:ident),*; $n:literal) => {
-        impl<$($node: IntoNode),*> NodeCollection for ($($node,)*) {
-            const N: usize = $n;
-            fn collect(self) -> [Option<Node>; Self::N] {
+        impl<$($node: IntoNodes),*> NodeCollection for ($($node,)*) {
+            fn collect(self) -> impl Iterator<Item = Nodes> {
                 let ($($node,)*) = self;
-                [$($node.into_node(),)*]
+                [$($node.into_nodes(),)*].into_iter()
             }
         }
     };
@@ -119,10 +108,15 @@ NodeCollection! { N1, N2, N3, N4, N5, N6, N7, N8, N9, N10, N11, N12, N13, N14, N
 
 
 macro_rules! native_tags {
-    ($($name:ident)*) => {$(
-        #[allow(non_upper_case_globals)]
-        pub const $name: Node = Node::new_element(stringify!($name));
-    )*};
+    ($($name:ident)*) => {
+        pub mod tag {
+            use super::Node;
+            $(
+                #[allow(non_upper_case_globals)]
+                pub const $name: Node = Node::new_element(stringify!($name));
+            )*
+        }
+    };
 } native_tags! {
     /* main root */
     html
