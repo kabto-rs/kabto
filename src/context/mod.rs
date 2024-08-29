2e0f1cec12372b242eca809a4fcb0c92d2413630
kana-rus <kanarus786@gmail.com>
2024-08-30 08:40:40 +0900

    @2024-08-30 08:40+9:00

--- src/context/mod.rs
+++ src/context/mod.rs
@@ -5,17 +5,17 @@ use crate::vdom::VDOM;
 
 
 pub(crate) struct Context {
-    current_vdom:  Option<VDOM>,
-    previous_vdom: Option<VDOM>,
-    document:      web_sys::Document,
-    effects:       Vec<Effect>,
+    current_fiber:  Option<VDOM>,
+    previous_fiber: Option<VDOM>,
+    document:       web_sys::Document,
+    effects:        Vec<Effect>,
 }
 
 impl Context {
     pub(crate) fn new() -> Self {
         Self {
-            current_vdom:  None,
-            previous_vdom: None,
+            current_fiber:  None,
+            previous_fiber: None,
             document:      crate::document(),
             effects:       Vec::new()
         }
@@ -26,7 +26,13 @@ impl Context {
     }
 
     pub(crate) fn insert(&mut self, vdom: VDOM) {
-        std::mem::swap(&mut self.current_vdom, &mut self.previous_vdom);
-        self.current_vdom = Some(vdom)
+        std::mem::swap(&mut self.current_fiber, &mut self.previous_fiber);
+        self.current_fiber = Some(vdom)
+    }
+
+    pub(crate) fn commit(&self) {
+        match (&self.current_fiber, &self.previous_fiber) {
+            todo!()
+        }
     }
 }
